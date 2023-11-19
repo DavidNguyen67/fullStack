4ee4b1d58c8dc810463c871967c08d991a25416e
PHAN MINH QUÂN <davidnguyen67@gmail.com>
2023-11-19 21:09:58 +0700

    base backEnd nestJs typescript

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app/middleware/controllers/app.controller';
+import { AppService } from './app/middleware/services/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './middleware/controllers/app.controller';
+import { AppService } from './middleware/services/app.service';
 
 @Module({
   imports: [],
