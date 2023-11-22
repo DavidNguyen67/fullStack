98f42dfc36d329861b227fe9e644c8093d1f2acf
PHAN MINH QUÂN <davidnguyen67@gmail.com>
2023-11-22 18:47:25 +0700

    base

--- src/components/authForm/LoginForm.tsx
+++ src/components/authForm/LoginForm.tsx
@@ -5,7 +5,6 @@ import {
   MDBIcon,
   MDBCheckbox,
   MDBTypography,
-  MDBValidation,
   MDBValidationItem,
   MDBCard,
   MDBCardBody,
@@ -13,130 +12,129 @@ import {
   MDBRow,
 } from 'mdb-react-ui-kit';
 import { NavLink } from 'react-router-dom';
+import { Typography } from '@mui/material';
 
 class LoginForm extends React.Component<{ width: string }> {
   render(): React.ReactNode {
     return (
-      <MDBValidation className={`${this.props.width} mx-auto`}>
-        <MDBRow className="d-flex justify-content-center align-items-center h-100">
-          <MDBCol col="12">
-            <MDBCard
-              className="bg-white my-5 mx-auto"
-              style={{ borderRadius: '1rem', maxWidth: '500px' }}
-            >
-              <MDBCardBody className="p-5 w-100 d-flex flex-column">
-                {/* <MDBValidationItem className="col-12"> */}
-                <h2 className="fw-bold mb-2 text-center">Sign in</h2>
-                {/* </MDBValidationItem> */}
-                <MDBValidationItem
-                  className="col-12"
-                  feedback="This field is required"
-                  invalid
-                >
-                  <MDBInput
-                    required
-                    size="lg"
-                    id="formControlLg"
-                    label="Email address"
-                  />
-                </MDBValidationItem>
-                <div className="my-2"></div>
-                <MDBValidationItem
-                  className="col-12"
-                  feedback="This field is required"
-                  invalid
-                >
-                  <MDBInput
-                    wrapperClass="mb-4 w-100"
-                    label="Password"
-                    id="formControlLg"
-                    type="password"
-                    size="lg"
-                    required
-                  />
-                </MDBValidationItem>
-                <div className="d-flex justify-content-between mx-3 my-1">
-                  <MDBCheckbox
-                    name="flexCheck"
-                    value=""
-                    id="flexCheckDefault"
-                    label="Remember me"
-                    required={false}
-                  />
-                  <MDBTypography
-                    style={{ color: '#3b71ca', cursor: 'pointer' }}
-                  >
-                    Forgot password
-                  </MDBTypography>
-                </div>
-                <MDBValidationItem className="col-12">
-                  <MDBBtn size="lg" className="col-12">
-                    Login
-                  </MDBBtn>
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <hr className="my-4" />
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <div className="text-center">
-                    <div className="d-flex justify-content-center">
-                      Not a member?
-                      <NavLink to={'/register'}>
-                        <MDBTypography
-                          style={{ color: '#3b71ca', cursor: 'pointer' }}
-                        >
-                          Register
-                        </MDBTypography>
-                      </NavLink>
-                    </div>
-                    <p>or sign up with:</p>
-                    <div
-                      className="d-flex justify-content-between mx-auto"
-                      style={{ width: '40%' }}
-                    >
-                      <MDBBtn
-                        tag="a"
-                        color="none"
-                        className="m-1"
-                        style={{ color: '#1266f1' }}
+      <MDBRow className="d-flex justify-content-center align-items-center h-100">
+        <MDBCol col="12">
+          <MDBCard
+            className="bg-white my-5 mx-auto"
+            style={{ borderRadius: '1rem', maxWidth: '500px' }}
+          >
+            <MDBCardBody className="p-5 w-100 d-flex flex-column">
+              {/* <MDBValidationItem className="col-12"> */}
+              <Typography variant="h2" className="fw-bold mb-2 text-center">
+                Sign In
+              </Typography>
+              {/* </MDBValidationItem> */}
+              <MDBValidationItem
+                className="col-12"
+                feedback="This field is required"
+                invalid
+              >
+                <MDBInput
+                  required
+                  size="lg"
+                  id="formControlLg"
+                  label="Email address"
+                />
+              </MDBValidationItem>
+              <div className="my-2"></div>
+              <MDBValidationItem
+                className="col-12"
+                feedback="This field is required"
+                invalid
+              >
+                <MDBInput
+                  wrapperClass="mb-4 w-100"
+                  label="Password"
+                  id="formControlLg"
+                  type="password"
+                  size="lg"
+                  required
+                />
+              </MDBValidationItem>
+              <div className="d-flex justify-content-between mx-3 my-1">
+                <MDBCheckbox
+                  name="flexCheck"
+                  value=""
+                  id="flexCheckDefault"
+                  label="Remember me"
+                  required={false}
+                />
+                <MDBTypography style={{ color: '#3b71ca', cursor: 'pointer' }}>
+                  Forgot password
+                </MDBTypography>
+              </div>
+              <MDBValidationItem className="col-12">
+                <MDBBtn size="lg" className="col-12">
+                  Login
+                </MDBBtn>
+              </MDBValidationItem>
+              <MDBValidationItem className="col-12">
+                <hr className="my-4" />
+              </MDBValidationItem>
+              <MDBValidationItem className="col-12">
+                <div className="text-center">
+                  <div className="d-flex justify-content-center">
+                    Not a member?
+                    <NavLink to={'/register'}>
+                      <MDBTypography
+                        style={{ color: '#3b71ca', cursor: 'pointer' }}
                       >
-                        <MDBIcon fab icon="facebook-f" size="sm" />
-                      </MDBBtn>
-
-                      <MDBBtn
-                        tag="a"
-                        color="none"
-                        className="m-1"
-                        style={{ color: '#1266f1' }}
-                      >
-                        <MDBIcon fab icon="twitter" size="sm" />
-                      </MDBBtn>
-
-                      <MDBBtn
-                        tag="a"
-                        color="none"
-                        className="m-1"
-                        style={{ color: '#1266f1' }}
-                      >
-                        <MDBIcon fab icon="google" size="sm" />
-                      </MDBBtn>
-
-                      <MDBBtn
-                        tag="a"
-                        color="none"
-                        className="m-1"
-                        style={{ color: '#1266f1' }}
-                      >
-                        <MDBIcon fab icon="github" size="sm" />
-                      </MDBBtn>
-                    </div>
+                        Register
+                      </MDBTypography>
+                    </NavLink>
                   </div>
-                </MDBValidationItem>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBValidation>
+                  <p>or sign up with:</p>
+                  <div
+                    className="d-flex justify-content-between mx-auto"
+                    style={{ width: '40%' }}
+                  >
+                    <MDBBtn
+                      tag="a"
+                      color="none"
+                      className="m-1"
+                      style={{ color: '#1266f1' }}
+                    >
+                      <MDBIcon fab icon="facebook-f" size="sm" />
+                    </MDBBtn>
+
+                    <MDBBtn
+                      tag="a"
+                      color="none"
+                      className="m-1"
+                      style={{ color: '#1266f1' }}
+                    >
+                      <MDBIcon fab icon="twitter" size="sm" />
+                    </MDBBtn>
+
+                    <MDBBtn
+                      tag="a"
+                      color="none"
+                      className="m-1"
+                      style={{ color: '#1266f1' }}
+                    >
+                      <MDBIcon fab icon="google" size="sm" />
+                    </MDBBtn>
+
+                    <MDBBtn
+                      tag="a"
+                      color="none"
+                      className="m-1"
+                      style={{ color: '#1266f1' }}
+                    >
+                      <MDBIcon fab icon="github" size="sm" />
+                    </MDBBtn>
+                  </div>
+                </div>
+              </MDBValidationItem>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
     );
   }
 }
--- src/components/authForm/SignUpForm.tsx
+++ src/components/authForm/SignUpForm.tsx
@@ -1,147 +1,209 @@
 import React from 'react';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Grid from '@mui/joy/Grid';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Button, TextField, Typography } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
 import {
   MDBBtn,
-  MDBInput,
-  MDBIcon,
   MDBCheckbox,
-  MDBTypography,
+  MDBIcon,
   MDBValidation,
   MDBValidationItem,
-  MDBCardBody,
-  MDBRow,
-  MDBCol,
-  MDBCard,
 } from 'mdb-react-ui-kit';
 import { NavLink } from 'react-router-dom';
-
 class SignUpForm extends React.Component<{ width: string }> {
+  state = {
+    value: '',
+  };
+  handleChange = (event: any) => {
+    this.setState({ value: event.target.value });
+    console.log(this.state.value);
+  };
+
   render(): React.ReactNode {
     return (
-      <MDBValidation className={`${this.props.width} mx-auto`}>
-        <MDBRow className="d-flex justify-content-center align-items-center h-100">
-          <MDBCol col="12">
-            <MDBCard
-              className="bg-white my-5 mx-auto"
-              style={{ borderRadius: '1rem', maxWidth: '500px' }}
-            >
-              <MDBCardBody className="p-5 w-100 d-flex flex-column">
-                <MDBValidationItem className="col-12">
-                  <h2 className="fw-bold mb-2 text-center">Sign up</h2>
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <MDBInput
-                    wrapperClass="mb-4 w-100"
-                    label="Email address"
-                    id="formControlLg"
-                    type="email"
-                    size="lg"
+      <>
+        <div className="my-4"></div>
+        <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+          <Grid xs={10} md={6} lg={4} sx={{ margin: 'auto' }}>
+            <MDBValidation>
+              <Card className="h-100">
+                <NavLink to={'/login'} className="d-flex">
+                  <Button variant="outlined" className="ms-auto">
+                    Login
+                    <LoginIcon />
+                  </Button>
+                </NavLink>
+                <Typography variant="h3" className="fw-bold mb-2 text-center">
+                  Register
+                </Typography>
+                <CardContent orientation="horizontal" sx={{ display: 'block' }}>
+                  <TextField
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
                     required
                   />
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <MDBInput
-                    wrapperClass="mb-4 w-100"
+                  <div className="my-2"></div>
+                  <TextField
                     label="Password"
-                    id="formControlLg"
-                    type="password"
-                    size="lg"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
                     required
                   />
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <MDBInput
-                    wrapperClass="mb-4 w-100"
+                  <div className="my-2"></div>
+                  <TextField
                     label="Confirm Password"
-                    id="formControlLg"
-                    type="password"
-                    size="lg"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
                     required
                   />
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <div className="d-flex justify-content-center mb-2">
-                    <MDBValidationItem
-                      className="col-12"
-                      feedback="You must agree before submitting."
-                      invalid
+                  <div className="my-2"></div>
+                  <FormControl fullWidth size="small">
+                    <InputLabel id="demo-select-small-label">Gender</InputLabel>
+                    <Select
+                      labelId="demo-select-small-label"
+                      id="demo-select-small"
+                      value={this.state.value}
+                      label="Gender"
+                      onChange={this.handleChange}
                     >
-                      <MDBCheckbox
-                        name="flexCheck"
-                        value=""
-                        id="flexCheckDefault"
-                        label="Subscribe to our newsletter"
-                        required
-                      />
-                    </MDBValidationItem>
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <div className="my-2"></div>
+                  <TextField
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                  <div className="my-2"></div>
+                  <TextField
+                    label="Address"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                  <div className="my-2"></div>
+                  <div className="d-flex">
+                    <FormControl sx={{ width: '48%' }} size="small">
+                      <InputLabel id="demo-select-small-label">
+                        TypeId
+                      </InputLabel>
+                      <Select
+                        labelId="demo-select-small-label"
+                        id="demo-select-small"
+                        value={this.state.value}
+                        label="TypeId"
+                        onChange={this.handleChange}
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                    <FormControl
+                      sx={{ width: '48%', marginLeft: 'auto' }}
+                      size="small"
+                    >
+                      <InputLabel id="demo-select-small-label">
+                        PositionId
+                      </InputLabel>
+                      <Select
+                        labelId="demo-select-small-label"
+                        id="demo-select-small"
+                        value={this.state.value}
+                        label="PositionId"
+                        onChange={this.handleChange}
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
                   </div>
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <NavLink to={'/login'}>
-                    <MDBTypography
-                      className="text-center"
-                      style={{ color: '#3b71ca', cursor: 'pointer' }}
-                    >
-                      Already has an account ?
-                    </MDBTypography>
-                  </NavLink>
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <div className="mb-2"></div>
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <MDBBtn className="w-100 mb-4" size="lg">
-                    Sign up
+                  <div className="my-2"></div>
+                  <TextField
+                    label="Avatar"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                  <div className="my-2"></div>
+                  <MDBValidationItem
+                    className="col-12"
+                    feedback="You must agree before submitting."
+                    invalid
+                  >
+                    <MDBCheckbox
+                      label="Agree to terms and conditions"
+                      id="invalidCheck"
+                      required
+                    />
+                  </MDBValidationItem>
+                  <div className="my-2"></div>
+                </CardContent>
+                <MDBBtn type="submit">Submit form</MDBBtn>
+                <div className="text-center">
+                  <p>or sign up with:</p>
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: '#1266f1' }}
+                  >
+                    <MDBIcon fab icon="facebook-f" size="sm" />
                   </MDBBtn>
-                </MDBValidationItem>
-                <MDBValidationItem className="col-12">
-                  <div className="text-center">
-                    <p>or sign up with:</p>
-                    <div
-                      className="d-flex justify-content-between mx-auto"
-                      style={{ width: '40%' }}
-                    >
-                      <MDBBtn
-                        tag="a"
-                        color="none"
-                        className="mx-3"
-                        style={{ color: '#1266f1' }}
-                      >
-                        <MDBIcon fab icon="facebook-f" size="sm" />
-                      </MDBBtn>
-                      <MDBBtn
-                        tag="a"
-                        color="none"
-                        className="m-1"
-                        style={{ color: '#1266f1' }}
-                      >
-                        <MDBIcon fab icon="twitter" size="sm" />
-                      </MDBBtn>
-
-                      <MDBBtn
-                        tag="a"
-                        color="none"
-                        className="m-1"
-                        style={{ color: '#1266f1' }}
-                      >
-                        <MDBIcon fab icon="google" size="sm" />
-                      </MDBBtn>
-
-                      <MDBBtn
-                        tag="a"
-                        color="none"
-                        className="m-1"
-                        style={{ color: '#1266f1' }}
-                      >
-                        <MDBIcon fab icon="github" size="sm" />
-                      </MDBBtn>
-                    </div>
-                  </div>
-                </MDBValidationItem>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBValidation>
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: '#1266f1' }}
+                  >
+                    <MDBIcon fab icon="twitter" size="sm" />
+                  </MDBBtn>
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: '#1266f1' }}
+                  >
+                    <MDBIcon fab icon="google" size="sm" />
+                  </MDBBtn>
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: '#1266f1' }}
+                  >
+                    <MDBIcon fab icon="github" size="sm" />
+                  </MDBBtn>
+                </div>
+              </Card>
+            </MDBValidation>
+          </Grid>
+        </Grid>
+      </>
     );
   }
 }
